af8c2bf70e0d6992e148cef0ed7741c86fe16039
Pon1147 <khoale1147@gmail.com>
2025-06-09 09:09:56 +0700

    refactor[project]: Change: - Update .prettierrc to specify "endofine": "LF". - Fix ESLint.config.js to use the magnetic configuration .prettierrc instead of overwriting. - Configure vs code to automatically format with prettier and use LF.

--- eslint.config.js
+++ eslint.config.js
@@ -1,46 +1,46 @@
 // @ts-check
-const eslint = require("@eslint/js");
-const tseslint = require("typescript-eslint");
-const angular = require("angular-eslint");
-const prettier = require("eslint-plugin-prettier");
-const eslintConfigPrettier = require("eslint-config-prettier");
+const eslint = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+const angular = require('angular-eslint');
+const prettier = require('eslint-plugin-prettier');
+const eslintConfigPrettier = require('eslint-config-prettier');
 
 module.exports = tseslint.config(
   {
-    files: ["**/*.ts"],
+    files: ['**/*.ts'],
     extends: [
       eslint.configs.recommended,
       ...tseslint.configs.recommended,
       ...tseslint.configs.stylistic,
       ...angular.configs.tsRecommended,
-      eslintConfigPrettier, // Add this to disable conflicting ESLint rules
+      eslintConfigPrettier,
     ],
     processor: angular.processInlineTemplates,
     plugins: {
-      prettier, // Register Prettier as a plugin
+      prettier,
     },
     rules: {
-      "prettier/prettier": "error", // Enforce Prettier formatting as an ESLint rule
-      "@angular-eslint/directive-selector": [
-        "error",
+      'prettier/prettier': 'error',
+      '@angular-eslint/directive-selector': [
+        'error',
         {
-          type: "attribute",
-          prefix: "app",
-          style: "camelCase",
+          type: 'attribute',
+          prefix: 'app',
+          style: 'camelCase',
         },
       ],
-      "@angular-eslint/component-selector": [
-        "error",
+      '@angular-eslint/component-selector': [
+        'error',
         {
-          type: "element",
-          prefix: "app",
-          style: "kebab-case",
+          type: 'element',
+          prefix: 'app',
+          style: 'kebab-case',
         },
       ],
     },
   },
   {
-    files: ["**/*.html"],
+    files: ['**/*.html'],
     extends: [
       ...angular.configs.templateRecommended,
       ...angular.configs.templateAccessibility,
---
+++ src/app/datalist/datalist.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-datalist',
+  imports: [],
+  templateUrl: './datalist.component.html',
+  styleUrl: './datalist.component.scss',
+})
+export class DatalistComponent {}
